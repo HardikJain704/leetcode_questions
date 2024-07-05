20941eb857258679766daa98305b964756743232
Hardik Jain <jainhardik2626.hj26@gmail.com>
2024-07-05 10:15:04 +0530

    reverse_words_in_string

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+pub fn reverse_words(s: String) -> String {
+        let words_array: Vec<&str> = s.split(" ").collect();
+        let mut res: Vec<&str> = vec![];
+        for i in (0..words_array.len()).rev(){
+            if words_array[i] != "" {
+                res.push(words_array[i])
+            }
+        }
+        res.join(" ")
+}
+
+#[cfg(test)]
+
+mod test{
+    use super::*;
+
+    #[test]
+    fn test_with_string_having_other_characters_also() {
+        let str = "the sky! $ is bl^ue@";
+        let res = reverse_words(str.to_owned());
+        assert_eq!("bl^ue@ is $ sky! the" , res);
+
+    }
+
+    #[test]
+    fn test_with_single_word(){
+        let str = "hardik!";
+        let res = reverse_words(str.to_owned());
+        assert_eq!("hardik!" , res);
+    
+    }
+
+}
+
+
+fn main() {
+    let str = "the sky! $ is blue!";
+    let res = reverse_words(str.to_owned());
+    println!("{:?}" , res);
+
+}
